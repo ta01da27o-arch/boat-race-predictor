652ff085f1742fec9bd9707796c073f087ce2b12
ta01da27o-arch <ta01da27o@gmail.com>
2026-01-14 15:16:06 +0900

    Add files via upload

--- data/stadiums.js
+++ data/stadiums.js
@@ -2,5 +2,8 @@ export const stadiums = [
   { id: 1, name: "桐生", feature: "イン有利・高速" },
   { id: 2, name: "戸田", feature: "差し有効" },
   { id: 3, name: "江戸川", feature: "荒水面" },
-  // ...最終的に24場すべて記載
+  { id: 4, name: "平和島", feature: "ダッシュ有効" },
+  { id: 5, name: "多摩川", feature: "水面安定" },
+  { id: 6, name: "浜名湖", feature: "差し巧者" }
+  // ※最終的に24場すべて追加
 ];
